12b3261f9a7c7895a678120df977fe22977a5c4c
bunnyrailgun <bunnyrailgun@gmail.com>
2016-09-10 13:10:14 -0400

    Added Text Box

--- src/MoodBot/model/Contact.java
+++ src/MoodBot/model/Contact.java
@@ -1,9 +1,13 @@
 package MoodBot.model;
 
+import java.util.ArrayList;
+
 public class Contact{
     public String name;
+    public ArrayList<String> messages;
     
     public Contact(String name){
         this.name = name;
+        messages = new ArrayList<>();
     }
 }
--- src/MoodBot/view/ChatWindowView.java
+++ src/MoodBot/view/ChatWindowView.java
@@ -4,9 +4,13 @@ import MoodBot.StartupConstants;
 import MoodBot.model.Contact;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -42,13 +46,36 @@ public class ChatWindowView{
         Contact Zaki = new Contact("Zaki");
         ContactView ZakiView = new ContactView(Zaki);
         contacts.getChildren().add(ZakiView);
+        scrollContacts.setHbarPolicy(ScrollBarPolicy.NEVER);
+        scrollContacts.setVbarPolicy(ScrollBarPolicy.ALWAYS);
         chatPane.setLeft(scrollContacts);
         
         //Create ChatWindow
         
         //Create Messages
         VBox messages = new VBox();
+        messages.getStyleClass().add("messages");
         ScrollPane scrollMessages = new ScrollPane(messages);
+        scrollMessages.getStyleClass().add("scroll_messages");
+        
+        //Create Input window
+        HBox inputBox = new HBox();
+        inputBox.getStyleClass().add("input_box");
+        
+        TextArea textArea = new TextArea();
+        Button sendButton = new Button("Send");
+        sendButton.getStyleClass().add("send_button");
+        
+        //Add to input box
+        inputBox.getChildren().add(textArea);
+        inputBox.getChildren().add(sendButton);
+        
+        //Add to chat window
+        chatWindow.getChildren().add(messages);
+        chatWindow.getChildren().add(inputBox);
+        
+        //Add chat window to chat pane
+        chatPane.setCenter(chatWindow);
         
         
         
